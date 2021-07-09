960e1d6bb5ddd678c0a6f52f88c90ae8540484e1
larrydor <86853201+larrydor@users.noreply.github.com>
2021-07-09 14:39:22 -0400

    Update game.py

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ print(x)
 if (x == "rock") or (x == "paper") or (x == "scissors") or (x == "ROCK") or (x == "Rock") or (x == "Paper") or (x == "PAPER") or (x == "Scissors") or (x == "SCISSORS"):
     print("VALID")
 else:
-    print("OOPS, INVALID ENTRY, PLEASE TRY AGAIN!")
+    print("Sorry, You have entered an Invalid Entry. PLEASE TRY AGAIN!")
     exit()
 
 # print("LATER MESSAGES")
@@ -39,26 +39,30 @@ c = random.choice(valid_options)
 print("COMPUTER CHOSE:", c)
 # print(random.choice(valid_options))
 
-# Winner
+# Determing Winner
+
 # Source: https://realpython.com/python-rock-paper-scissors/
 
 if x == c:
-    print("Both players selected {x}. It's a tie!")
+    print("Both players selected {x}. It's a tie, their is no winner! Let's Try Again")
 elif (x == "rock") or (x == "ROCK") or (x == "Rock"):
     if c == "scissors":
-        print("Rock smashes scissors! You win!")
+        print("Rock smashes scissors! You are the winner!")
     else:
-            print("Paper covers rock! You lose.")
+            print("Paper covers rock! Sorry you lost.")
 elif (x == "paper") or (x == "PAPER") or (x == "Paper"):
     if c == "rock":
-        print("Paper covers rock! You win!")
+        print("Paper covers rock! You are the winner!")
     else:
-        print("Scissors cuts paper! You lose.")
+        print("Scissors cuts paper! Sorry you lost.")
 elif (x == "scissors") or (x == "SCISSORS") or (x == "Scissors"):
     if c == "paper":
-        print("Scissors cuts paper! You win!")
+        print("Scissors cuts paper! You are the winner!")
     else:
-        print("Rock smashes scissors! You lose.")
+        print("Rock smashes scissors! Sorry you lost.")
 
 print("Thank You For Participating In Our Game!!!")
-print("Please Play Again Soon!")
+again = x("Would you like to play another round?")
+
+# print("Please Play Again Soon!")
+exit()
